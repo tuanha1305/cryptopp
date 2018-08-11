ac3c971312b7ff931d56eae9717f446daa3b0f07
Jason Dictos <jason@dictos.com>
2018-08-10 20:26:44 -0400

    Some fixes for building on linux

--- queue.h
+++ queue.h
@@ -148,7 +148,7 @@ NAMESPACE_END
 
 #ifndef __BORLANDC__
 NAMESPACE_BEGIN(std)
-template<> inline void swap(CryptoPP::ByteQueue &a, CryptoPP::ByteQueue &b) noexcept
+template<> inline void swap(CryptoPP::ByteQueue &a, CryptoPP::ByteQueue &b)
 {
 	a.swap(b);
 }
